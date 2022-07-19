623466e0a1a29486edb029542e763d8ebd8be11d
Juan Carlos <jchernandez827@gmail.com>
2022-07-18 21:40:25 -0500

    Add header and main text

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,11 @@ import "../styles/main.css";
 
 const Main = () => (
   <div className="main-container">
-    <h1>asdasfdsfgg</h1>
+    <h1>Tenzies</h1>
+    <p>
+      Roll until all dice are the same. Click each die to freeze it at its
+      current value between rolls.
+    </p>
   </div>
 );
 
